26d3b7bc1d2f485ac412753ccfc8af7b58a195d9
David Gibran Maulana <139204443+DavidGibran@users.noreply.github.com>
2025-06-03 14:49:44 +0700

    Add files via upload

---
+++ Heptaseas/product_slider.php
@@ -0,0 +1,116 @@
+<section class="product-slider">
+
+    <!-- heading -->
+    <div class="slider-heading">
+        <h3>New Arrival.<span> Get Yours</span></h3>
+    </div>
+
+
+    <ul id="autoWidth" class="cs-hidden">
+        <li class="item-a">
+            <!-- Product Box 1 -->
+            <div class="product-box">
+                <a href="#">
+                    <img src="./asset/tshirt1.jpeg" alt="kaos1-Beer">
+                    <div class="card-text">
+                        <strong> T-shirt </strong>
+                        <div class="buy-price">
+                            <p class="card-text">Happy Days Cheers</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </li>
+
+        <li class="item-a">
+            <!-- Product Box 1 -->
+            <div class="product-box">
+                <a href="#">
+                    <img src="./asset/thsirt-6.jpeg" alt="kaos1-Beer">
+                    <div class="card-text">
+                        <strong> T-shirt </strong>
+                        <div class="buy-price">
+                            <p class="card-text">Cyber Text</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </li>
+
+        <li class="item-a">
+            <!-- Product Box 1 -->
+            <div class="product-box">
+                <a href="#">
+                    <img src="./asset/tshirt.5.jpeg" alt="kaos1-Beer">
+                    <div class="card-text">
+                        <strong> T-shirt </strong>
+                        <div class="buy-price">
+                            <p class="card-text">Osaka-Tora</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </li>
+
+        <li class="item-a">
+            <!-- Product Box 1 -->
+            <div class="product-box">
+                <a href="#">
+                    <img src="./asset/tshirt-4.jpg" alt="kaos1-Beer">
+                    <div class="card-text">
+                        <strong> T-shirt </strong>
+                        <div class="buy-price">
+                            <p class="card-text">Angels Never Die</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </li>   
+
+        <li class="item-a">
+            <!-- Product Box 1 -->
+            <div class="product-box">
+                <a href="#">
+                    <img src="./asset/hoodie-1.jpeg" alt="kaos1-Beer">
+                    <div class="card-text">
+                        <strong> Hoodie </strong>
+                        <div class="buy-price">
+                            <p class="card-text">Cyber Crime</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </li>   
+
+        <li class="item-a">
+            <!-- Product Box 1 -->
+            <div class="product-box">
+                <a href="#">
+                    <img src="./asset/hoodie-2.jpeg" alt="kaos1-Beer">
+                    <div class="card-text">
+                        <strong> Hoodie </strong>
+                        <div class="buy-price">
+                            <p class="card-text">Still Toddler</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </li>   
+
+        <li class="item-a">
+            <!-- Product Box 1 -->
+            <div class="product-box">
+                <a href="#">
+                    <img src="./asset/hoodie-3.jpg" alt="kaos1-Beer">
+                    <div class="card-text">
+                        <strong> Hoodie </strong>
+                        <div class="buy-price">
+                            <p class="card-text">Pioneers</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </li>   
+
+    </ul>
+</section>
